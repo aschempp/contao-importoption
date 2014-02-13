e90003d898e548411d58eaf18bc71c392759b8c6
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-02-13 16:34:59 +0100

    Added Contao 3 autoload files

---
+++ config/autoload.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * importoption Extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2014, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       http://github.com/aschempp/contao-importoption
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'ImportOptionWizard' => 'system/modules/importoption/ImportOptionWizard.php',
+));
